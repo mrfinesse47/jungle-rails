bf29f0af146fd642110a4b7f3fcd85fe59957ee8
Kevin Mason <krmason2020@gmail.com>
2022-01-06 18:32:38 +0000

    basic table to show categories working

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,14 @@
 class Admin::CategoriesController < ApplicationController
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+    end
+  end
+
   def index
+    @categories = Category.order(id: :asc).all
   end
 
   def new
