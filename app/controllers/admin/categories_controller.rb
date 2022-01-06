68c1614bd42dca9a67c9e9242bee9693936aff01
Kevin Mason <krmason2020@gmail.com>
2022-01-06 22:24:22 +0000

    sales controller and view created

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,7 @@
 class Admin::CategoriesController < ApplicationController
   before_filter :authenticate
 
-  def authenticate
-    authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == ENV['USERNAME'] && password == ENV['PASSWORD']
-    end
-  end
+ 
 
   def index
     @categories = Category.order(id: :asc).all
