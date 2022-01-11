55abb003d5bbd2dd1f02911c00d69c7d5285cf22
Kevin Mason <krmason2020@gmail.com>
2022-01-11 21:45:21 +0000

    initial setup for testing

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -21,18 +21,15 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
       visit root_path
       expect(page).to have_css 'article.product', count: 1
     end
-
-    scenario "The Details link should be visible" do
-      visit root_path
-      expect(page).to have_link("Details", visible: true)
-    end
-
-    scenario "Should be able to click on the Details link and show the product" do
-      visit root_path
-      click_on 'Details'
-      expect(page).to have_css '.product-detail', count: 1
     
+    scenario "The add to cart button should be visible" do
+      visit root_path
+      puts page.html
+      expect(page).to have_button("Add", visible: true)
+
     end
+
+  
 
     
 end
